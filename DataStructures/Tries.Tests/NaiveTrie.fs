b90fb7acd5e674bda272e705af40762e424c77e2
George Shaikovski <g.shaikov@gmail.com>
2020-09-03 00:45:46 +0100

    DataStructures: add naive trie

--- DataStructures/Tries.Tests/NaiveTrie.fs
+++ DataStructures/Tries.Tests/NaiveTrie.fs
@@ -11,14 +11,54 @@ module Lookup =
         let trie = Empty
 
         [<Fact>]
-        let ``not found`` () = lookup 0uy trie |> StrictEqual None
+        let ``not found`` () = lookup trie "abc" |> StrictEqual None
 
-    module Onelevel =
+    module OneNode =
 
-        let trie = Leaf(15uy, 42)
+        let trie = Branch(Some 42, [])
 
-        [<Fact>]
-        let ``not found`` () = lookup 0uy trie |> StrictEqual None
+        let testCases: obj [] seq =
+            seq {
+                yield [| ""; Some 42 |]
+                yield [| "a"; None |]
+                yield [| "ab"; None |]
+            }
 
-        [<Fact>]
-        let ``found 42`` () = lookup 15uy trie |> StrictEqual(Some 42)
+        [<Theory; MemberData("testCases")>]
+        let ``one node`` (key: Key, value: Value<int>) = lookup trie key |> StrictEqual value
+
+    module ThreeNodes =
+
+        let trieTwoLevels =
+            Branch
+                (Some(99),
+                 [ ('a', Branch(Some 42, []))
+                   ('b', Branch(Some 18, [])) ])
+
+        let testCasesTwoLevels: obj [] seq =
+            seq {
+                yield [| ""; Some(99) |]
+                yield [| "a"; Some(42) |]
+                yield [| "b"; Some(18) |]
+                yield [| "ab"; None |]
+            }
+
+        [<Theory; MemberData("testCasesTwoLevels")>]
+        let ``two levels`` (key: Key, value: Value<int>) =
+            lookup trieTwoLevels key |> StrictEqual value
+
+        let trieThreeLevels =
+            Branch(Some(99), [ ('a', Branch(Some 42, [ ('b', Branch(Some 18, [])) ])) ])
+
+        let testCasesThreeLevels: obj [] seq =
+            seq {
+                yield [| ""; Some(99) |]
+                yield [| "a"; Some(42) |]
+                yield [| "b"; None |]
+                yield [| "ab"; Some(18) |]
+                yield [| "abc"; None |]
+            }
+
+        [<Theory; MemberData("testCasesThreeLevels")>]
+        let ``three levels`` (key: Key, value: Value<int>) =
+            lookup trieThreeLevels key |> StrictEqual value
--- DataStructures/Tries/NaiveTrie.fs
+++ DataStructures/Tries/NaiveTrie.fs
@@ -1,11 +1,27 @@
 module Tries.NaiveTrie
 
-type Prefix = byte
-type Mask = byte
+type Key = string
+type KeyAtom = char
+type Value<'a> = Option<'a>
 
-type Trie =
-    | Branch of Prefix * Mask * Trie * Trie
-    | Leaf of Prefix * int
+type Trie<'a> =
     | Empty
+    | Branch of Value<'a> * (KeyAtom * Trie<'a>) list
 
-let lookup key trie = Some 0
+let lookup trie key =
+    let rec processNode trie key =
+        match Seq.tryHead key, trie with
+        | _, Empty -> None
+        | None, Branch (value, _) -> value
+        | Some keyAtom, Branch (_, children) -> Seq.tail key |> processChildren children keyAtom
+
+    and processChildren lon keyAtom keyRemaining =
+        match lon with
+        | [] -> None
+        | (_, Empty) :: rest -> None
+        | (atom, nextTrie) :: rest ->
+            if keyAtom = atom
+            then processNode nextTrie keyRemaining
+            else processChildren rest keyAtom keyRemaining
+
+    processNode trie key
